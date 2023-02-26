37ebd6b050e68b9507a017810334ab0cb768df19
savannah-yahsuanlin <savannahwang75@gmail.com>
2023-02-27 00:09:22 +0800

    new, show, index, html

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,22 +1,33 @@
 class PicsController < ApplicationController
+	before_action :find_pic, only: [:show, :edit, :update, :destroy] 
 
 	def index
-		@pics = Pic.all()
-	end
-
-	def new
-		@pic = Pic.new()
-	end
-
-	def create
-		@pic = Pic.new(pic_params)
+		@pics = Pic.all
 	end
 
 	def show
 	end
 
+	def new
+		@pic = Pic.new
+	end
+
+	def create
+		@pic = Pic.new(pic_params)
+
+		if @pic.save
+			redirect_to @pic, notice: "Yes! It was saved!"
+    else
+      render 'new'
+    end
+	end
+
 	private
 	def pic_params
 		params.require(:pic).permit(:title, :description)
+	end
+
+	def find_pic
+		@pic = Pic.find(params[:id])
 	end
 end
