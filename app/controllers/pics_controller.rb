e195c6c6c30d31e856c44023657973eaa17a13bd
savannah-yahsuanlin <savannahwang75@gmail.com>
2023-02-26 23:52:28 +0800

    create model

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,5 +1,22 @@
 class PicsController < ApplicationController
 
 	def index
+		@pics = Pic.all()
+	end
+
+	def new
+		@pic = Pic.new()
+	end
+
+	def create
+		@pic = Pic.new(pic_params)
+	end
+
+	def show
+	end
+
+	private
+	def pic_params
+		params.require(:pic).permit(:title, :description)
 	end
 end
